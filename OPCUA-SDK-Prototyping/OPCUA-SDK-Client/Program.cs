dceb15c64a06fa8150d0101858018f0dcfdd0196
SzilvasiPeter <peti.szilvasi95@gmail.com>
2021-11-11 18:35:53 +0100

    Create OPCUA client to receive temperature node.

---
+++ OPCUA-SDK-Prototyping/OPCUA-SDK-Client/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Opc.UaFx.Client;
+
+namespace OPCUA_SDK_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string endpoint = "opc.tcp://localhost:5555/Temperature";
+            using (var client = new OpcClient(endpoint))
+            {
+                client.Connect();
+
+                while (true)
+                {
+                    var temperature = client.ReadNode("ns=2;s=Temperature");
+                    Console.WriteLine("Temperature output value: {0}", temperature);
+
+                    Task.Delay(1000).Wait();
+                }
+            }
+        }
+    }
+}
